421b2aaf896f4c66bf2388d198da133920dbbb4d
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-10-14 19:14:03 +0530

    first commit

---
+++ arithmetic_op.py
@@ -0,0 +1,20 @@
+# Arithmetic Operators:-
+    # To Perform Arithmetic Operation we do use
+    # Arithmetic Operators.
+
+x = 7
+y = 4
+
+# Addition -->   +
+addition = x + y
+print(f"Addition: {x} + {y} = {addition}")
+
+# Substraction -->   -
+substraction = x - y
+print(f"")
+# Multiplication -->   *
+# Division 
+    # True Division -->   /
+    # Floor Division -->   //
+# Power -->   **
+# Mod -->   %
---
+++ scratch.py
@@ -0,0 +1,53 @@
+salalry = 2000
+name = 'kumar'
+# print(name, salalry)
+# print(name +" salary is",+ salalry)
+# print(f"{name} salary is {salalry}")
+
+students = [
+    {
+        'firstname': "Farah",
+        'lastname': 'khan',
+        'dob': '10/10/1990',
+        'city': 'Hyderabad'
+    },
+    {
+        'firstname': "Sana",
+        'lastname': 'Kureshi',
+        'dob': '11/11/1980',
+        'city': 'Secundarabad'
+    }
+]
+
+
+# Hello, Myself farah khan, I am from hyderabad, born in 1990
+# print(f"Hello, Myself {students[0]['firstname']} {students[0]['lastname']}, I am from {students[0]['city']}, born in {students[0]['dob'][-4::]}.")
+# print(f"Hello, Myself {students[1]['firstname']} {students[1]['lastname']}, I am from {students[1]['city']}, born in {students[1]['dob'][-4::]}.")
+
+
+
+list1 = [
+    'name',
+    'age',
+    'hobby',
+    'nationality'
+]
+
+list2 = [
+    'Sanjana',
+    30,
+    'Making Reels',
+    'Indian'
+]
+
+# Hello Buddy.., My name is Sanjana, My age is 30, My hobby is Making Reels, My nationality is Indian.
+
+
+about = "We are IITians, from Delhi."
+kiran = {
+    'fullname': "Kiran Mujumdar",
+    'age': 22,
+    'course': 'Computer Science'
+}
+
+# Hello I am Kiran M. from Delhi, Studying Computer Science in IIT.
