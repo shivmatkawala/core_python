9fd8d74d5bb8469f214335c0b3a4553d3f29b596
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-10-15 19:18:36 +0530

    comparison operator

--- arithmetic_op.py
+++ arithmetic_op.py
@@ -11,10 +11,25 @@ print(f"Addition: {x} + {y} = {addition}")
 
 # Substraction -->   -
 substraction = x - y
-print(f"")
+print(f"Substraction: {x} - {y} = {substraction}")
+
 # Multiplication -->   *
+multiplication = x * y
+print(f"Multiplication: {x} * {y} = {multiplication}")
+
 # Division 
     # True Division -->   /
+true_division = x / y
+print(f"True Division: {x} / {y} = {true_division}")
+
     # Floor Division -->   //
+floor_division = x // y
+print(f"Floor Division: {x} // {y} = {floor_division}")
+
 # Power -->   **
+power = x ** y
+print(f"Power: {x} ** {y} = {power}")
+
 # Mod -->   %
+mod = x % y
+print(f"Mod: {x} % {y} = {mod}")
---
+++ comparison_op.py
@@ -0,0 +1,104 @@
+# COMPARISON OPERATORS:-
+
+# > --> Greater than
+# < --> Lesser than
+# >= --> Greater than or Equals to
+# <= --> Lesser than or equals to
+# != --> Not equals to
+# == equals to
+
+
+# Equals to -->    ==
+
+# x = 5
+# y = 5
+# print(x == y)
+
+# m = 10
+# n = 15 - 5
+# print(m == n)
+
+
+# z = 1
+# q = True
+# print(z == q)
+
+# d = [1, 2, 3, 4, 5]
+# e = 7
+# print(d[-1]+2 == e)
+
+# print(len(d) == 5)
+
+
+# name = "Laila"
+# print(name[1] == name[-1])
+
+# fruit = 'Apple'
+# print(fruit[0] == chr(65))
+
+
+# f = '8'
+# k = 8
+# print(f == k)
+
+#------------------------------------------------
+
+# a = 5
+# b = 10
+# print(a != b)
+# print(a == b)
+
+# j = 5.0
+# l = 5
+# print(j == l)
+
+
+# m = 5+0j
+# n = 5
+# o = 5.0
+# p = '5'
+# print(m != n != o != p)
+
+#-------------------------------
+
+# print('Hello' == 'olleH'[::-1])
+# print("BOB" == 'BOb'.upper())
+
+# s1 = "MADAM"
+# print(s1 == s1[::-1])
+
+
+# -----------------------------
+
+# print('ABCD' < 'abcd')
+# print('A' > chr(50))
+
+# print('hpple' < 'grapes')
+
+# print( 5 > 10)
+# print(10 > 5)
+# print(100 > 500-400)
+
+# print(True * True * True > 3)
+
+# print(70 > ord('A'))
+
+# set1 = {100, 200, 300}
+# set2 = {400, 500, 600}
+# print(set1[2] == set2[2]- 300)
+
+
+# k = 90
+# l = 100
+# m = 90
+# print(k >= l >= m)
+
+
+# print(90 >= 60 > 50 <= 50 != 40 >= 20)
+
+# print((5*4-3+True) > (3*3-5**2))
+
+# print(ord('a') -32 == ord('A'))
+
+# print(65 == 'A')
+
