878c4c0787000c669a0f58e2a710c18d1dbc3378
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-10-17 19:03:23 +0530

    saturday before diwali

--- logical_op.py
+++ logical_op.py
@@ -1,10 +1,10 @@
 # Logical Operators:-
 
-str1 = "HELLO"
-print("H" and "O" in str1)
-print("E" and "L" and "O" and "h" in str1)
+# str1 = "HELLO"
+# print("H" and "O" in str1)
+# print("E" and "L" and "O" and "h" in str1)
 
-print(str1 and str1[::-1] == str1[::-1])
+# print(str1 and str1[::-1] == str1[::-1])
 
 # print(5 and 6)
 # print(10 and 50)
@@ -24,10 +24,110 @@ print(str1 and str1[::-1] == str1[::-1])
 # list1 = [1, 2, 3, 4, 5, 6]
 # print((4 in list1 and 3 in list1) == True)
 
-
-
-
-
 # print(False and False)
 # print(5 and 0 == 0)
+
+
+print("--------------------------------------")
+
+# tup1 = (1, 2.2, 3+6j, 'baby', [12,34,65], ('k',2), True, False)
+# print(tup1[6::-2])
+
+# OR :-
+
+# x = 5
+# y = 10
+# print(x+y == 15 or x+y == 15)
+
+
+# y = 5-4
+# print(y == 0 or y == True)
+
+import copy
+# str1 = "NAYANTARA"
+# str2 = copy.deepcopy(str1)
+# print(id(str1), id(str2))
+# print(str1 == str2)
+# print(str1 is str2)
+# print(str1 == "NAYANTARA"[::-1][::-1] or  str1 == str2)
+
+# print("----------------------------------------")
+
+# l1 = [1, 2, 3, "Hello", True, 4.5]
+# l2 = copy.deepcopy(l1)
+# print(id(l1), id(l2))
+# print(l1 == l2)
+# print(l1 is l2)
+# print(l1 == l2)
+
+print("----------------------------------------")
+
+# print(False or True)
+
+# print(False or False)
+
+# print(True or True)
+
+# print(-0 or 0)
+
+# print(-5 or 5)
+# print(3 or 7)
+# print(9 or 5)
+
+
+# print(0 and "Hello")
+
+# print(False or False)
+# print(True or True)
+# print(False or True)
+# print(True or False)
+
+# m = (1, 2, 3)
+# n = copy.deepcopy(m)
+# print(id(m), id(n))
+
+# print(m is n or n is m)
+
+# set1 = {1, 2, 3}
+# set2 = copy.deepcopy(set1)
+# print(set1 is set2 or set2 == set1)
+# print(set1 is set2 and set1 == set2)
+
+
+ # not
+ 
+ 
+# x = 5
+# y = 5
+# print(id(x), id(y))
+
+# print(x is not y)
+# print()
+
+
+# m = [1, 2, 3]
+# n = [1, 2, 3]
+
+# print(id(m), id(n))
+
+# p = (1, 2, 3)
+# q = (1, 2, 3)
+# print(id(p), id(q))
+
+
+# z = (1, 2, 3, [10, 20, 30])
+# t = (1, 2, 3, [10, 20, 30])
+# print(id(z), id(t))
+
+
+# c = "Hello"
+# d = "Hello"
+# c = c.upper()
+
+# print(id(c), id(d))
+
+
+# print("Hello" is not "Hello")
+# print(5 != 5)
+
 
