86e2cce62a4c8305700c4dd1787c712e15165143
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-10-22 19:08:47 +0530

    completed assignment ternary

---
+++ assignment_op.py
@@ -0,0 +1,97 @@
+# ASSIGNMENT OPERATORS:-
+
+# x = 5   # Assigned 5 to x
+# x = "Hello"
+# x = 5.5
+# x = [12, 2,3 ,3,4 ]
+# x = True
+
+# print(x)
+
+
+#-------------------
+
+# x = 100
+
+# Sustract and assign:-
+# x = x - 10
+# x = x - 15
+# x = x - 50
+
+# x-=10
+# x-=15
+# x-=50
+# print(x)
+
+
+# add and assign
+# x = 100
+# x= x+10
+# x = x+15
+# x = x+50
+
+# x+=10
+# x+=15
+# x+=50
+# print(x)
+
+
+# Multiply and assign
+
+# x = 5
+
+# x = x * 3
+# x = x * 4
+# x = x * 4.5
+
+# x *=3
+# x *=4
+# x *=5.5
+# print(x)
+
+
+# true_divide and assign
+
+# x = 625
+
+# x = x / 15
+# x = x / 7
+
+# x /=5
+# x /=15
+
+# print(x)
+
+
+# Floor Divide and assign
+
+# x = 12321
+
+# x = x // 11
+# x = x // 5
+# x = x // 8
+
+# x //= 13
+# x //= 7
+# print(x)
+
+
+
+# power and assign
+
+# x = 0.5
+# x = x ** 2
+# x = x ** 3
+
+# x = 6
+# x = x ** 2
+# x =  x ** 3
+
+# print(x)
+
+
+# x = 200
+# x = x % 7
+# x = x % 2.5
+
+# print(x)
---
+++ ternary_op.py
@@ -0,0 +1,36 @@
+# Ternary Operator
+
+# x = 100
+
+# # I want to print "Big" if number is bigger than 20
+# # else print "Small"
+
+# ans = "Big" if x > 20 else "Small"
+# print(ans)
+
+
+# color = "Yellow"
+
+# ret = "Sacrifice" if color.title() == "Red" else "Nature"
+# print(ret)
+
+
+
+# age = eval(input("Enter your age: "))
+
+# eligibility = "Eligible for Voting" if age >= 18 else "Not eligible for voting"
+# print(eligibility)
+
+
+# Ask student to enter marks
+
+# if marks < 40 -->  fail
+# if marks >= 40 and marks < 60  -- pass
+# if marks >=60 and marks < 80 -- first class
+# if marks >= 80 and marks < 100 --> distingtion 
+
+
+# marks = eval(input("Enter marks: "))
+
+# result = "Fail" if marks < 40 else "Pass" if marks >= 40 and marks <60 else "First Class" if marks >= 60 and marks < 80 else "Distingtion" if marks >=80 and marks <100 else "Incorrect Marks"
+# print(result)
