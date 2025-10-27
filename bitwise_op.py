123941131b5f8c45c51ac0b8ac06eda335721340
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-10-27 19:26:25 +0530

    bitwise completed

---
+++ bitwise_op.py
@@ -0,0 +1,46 @@
+# BITWISE OPERATORS:-
+
+# char = "5"
+# print(ord("5"))
+
+# print(bin(53))
+
+# char = "d"
+# print(ord("d"))
+# print(bin(100))
+
+# char = "D"
+# print(ord("D"))
+# print(bin(68))
+
+# char = "#"
+# print(ord("#"))
+# print(bin(35))
+
+# 1000011
+print(int("1000011", 2))
+
+    # BIT is the smallest unit of memory.
+
+# AND Operation  &
+# print(73 & 56)
+
+# OR Operation   |
+# print(73 | 56)
+
+# XOR Operation   ^
+# print(73 ^ 56)
+
+# Left Shift Operation
+# print(56 << 2)
+
+# Right Shift Operation
+# print(56 >>6)
+
+
+
+num1 = 896
+num2 = 678
+
+
+
