40c78fba2f6c30390954e5fd1b7d9fc3f5619b79
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-10-29 19:04:15 +0530

    day 2 conditional statements

--- conditional_st.py
+++ conditional_st.py
@@ -106,3 +106,91 @@ dict_of_state_capitals = {
 
 # else:
 #     print("Odd")
+
+
+#----------------------------------------------------
+
+# IF  ---> only one time and for first condition it can be used
+# ELIF --> multiple times and except for first condition it can be used
+# ELSE --> only one time and last condition it can be defined
+
+# thief and police:-
+    # thief --> lost
+    # police --> won
+
+# role = input("what do you want to be: ")
+
+# if role == "police":    # condition1
+#     print("Won")
+# else:                   
+#     print("Lost")
+
+
+# Ask user to enter the name:-
+    # check if name has at least 5 charecter.
+    # if exact 5 charecters --> print ("Good Name")
+    # if less then 5 charecters --> print("short name")
+    # if more than 5 charecters --> print("Big name")
+    # if user didnt enter any name ---> print("Dont you have a name ?")
+
+# username = input("Enter your name: ")
+
+# if len(username) == 5:
+#     print("Best name")
+
+# elif len(username) > 5:
+#     print("Big name")
+
+# elif len(username) < 5 and len(username) > 0:
+#     print("Small name")
+
+# else:
+#     print("Dont you have a name ?")
+
+
+#------------------------------------------------------
+
+# ask  user  to enter a number 
+    # check if number is positive or negative or zero
+        # if positive print (entered positive number.)
+        # if negative prinnt(entered negative number.)
+        # if 0 print(enetered 0)
+
+# num = int(input("Enter a number: "))
+
+# if num == 0:
+#     print("You have entered 0")
+# elif num > 0:
+#     print("You have entered positive number")
+# elif num < 0:
+#     print("you have entered ngative number.")
+# else:
+#     print("You entered nothing.")
+
+
+# ask user to enter a string
+    # if string is a palindrome 
+        # print Palindrome
+    # if string is not a palindrome
+        # print No palindrome
+    
+# BOB  --> BOB
+# MADAM --> MADAM
+# 12321 --> 12321
+
+# str1 = input("Enter a string: ")
+
+# if str1 == str1[::-1]:
+#     print("Palindrome")
+
+# else:
+#     print("No Palindrome")
+
+
+
+l1 = [12, 34, 56 ,78 ,90, 45]
+
+# write a program which will aanalyze l1 list
+# if the difference between the biggest number of l1 
+# and smallest number of l1 is greater than 50 --> "huge difference"
+# if difference is less than 50 or equal to 50 then say "small difference"
