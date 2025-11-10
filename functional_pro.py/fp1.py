441c344a94bcbd9dd5675df8ca5bc6a6491003f2
shivmatkawala <shivkumarmatkawala@gmail.com>
2025-11-10 19:23:03 +0530

    started functions

---
+++ functional_pro.py/fp1.py
@@ -0,0 +1,79 @@
+# Procedural Programming Paradigm:
+
+# # Write a program to print the string in reverse order.
+# str1 = input("Enter a string: ")
+# print(str1[::-1])
+
+# # write a program which will take integer and print 
+# # whether it is even or odd.
+
+# num = int(input("Enter a number: "))
+# if num % 2 == 0:
+#     print("Even")
+# else:
+#     print("odd")
+
+# # write a program which will greet you.
+# print("Hello Shalini.!")
+
+# Functional Programming Paradigm:-
+
+# # Write a program to print the string in reverse order.
+# def rev_string(str1):    # Header of the function
+#     print(str1[::-1])
+
+# rev_string("Kumar")
+# rev_string("Harika")
+# rev_string("Sanjana")
+# rev_string("Karthik")
+
+# # write a program which will take integer and print 
+# # whether it is even or odd.
+
+# def even_or_odd(num):
+#     if num % 2 == 0:
+#         print("Even")
+#     else:
+#         print("Odd")
+
+# # even_or_odd(33)
+
+# # write a program which will greet you.
+
+# def greet():
+#     print("Hello Shalini..!")
+
+# # greet()     # call to the function
+
+
+#----------------------------PRACTICE------------------------------
+
+#1) Write a function which prints table of any number.
+# def get_table(num):
+#     for i in range(1, 11):
+#         print(f"{num} * {i} = {num * i}")
+
+# get_table(34)
+
+#2) Write a function which will print Alphabetic 
+   #charecters ascii code
+
+
+#3) Write a function which does concatinnation of two different 
+# strings
+
+#4) Write a function which will convert any string to title string.
+
+#5) write a function which will print todays date.
+# import datetime
+# print(datetime.datetime.today())
+
+# write a function which will take two different numbers as attributes
+# and print sum, multipliation, division and substraction.
+
+# write a function which will take a number and find if the number 
+# is even or odd, int or float.
+
+
+# write a function which will take a tuple as an attribute
+# and find the mode of it.
